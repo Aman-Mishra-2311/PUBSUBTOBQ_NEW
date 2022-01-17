50e9d85068cd6cd4cad4631260889537ea628d1c
Aman Mishra <aman.mishra@nttdata.com>
2022-01-18 00:13:52 +0530

    Second Commit:DLQ FILTER

--- src/main/java/Streaming.java
+++ src/main/java/Streaming.java
@@ -8,22 +8,40 @@ import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TupleTagList;
 
 public class Streaming {
 
     public static  void main(String args[]){
+         TupleTag<String> VALID_ROWS=new TupleTag<>(){};
+        TupleTag<String> INVALID_ROWS=new TupleTag<>(){};
         DataflowPipelineOptions dataflowPipelineOptions= PipelineOptionsFactory.as(DataflowPipelineOptions.class);
         dataflowPipelineOptions.setJobName("StreamingIngestion");
         dataflowPipelineOptions.setProject("nttdata-c4e-bde");
         dataflowPipelineOptions.setRegion("europe-west4");
         dataflowPipelineOptions.setGcpTempLocation("gs://c4e-uc1-dataflow-temp-15/temp");
         dataflowPipelineOptions.setRunner(DataflowRunner.class);
+
         Pipeline pipeline= Pipeline.create(dataflowPipelineOptions);
-        PCollection<String> pubsubmessage=pipeline.apply(PubsubIO.readStrings().fromTopic("projects/nttdata-c4e-bde/topics/uc1-input-topic-15"));
-        PCollection<TableRow> bqrow=pubsubmessage.apply(ParDo.of(new ConvertorStringBq()));
+
+        String dlqTopicName="projects/nttdata-c4e-bde/topics/uc1-dlq-topic-15";
+        String subscriptionData="projects/nttdata-c4e-bde/subscriptions/uc1-input-topic-sub-15";
+        PCollection<String> pubsubmessage=pipeline.apply("ReadMessage", PubsubIO.readStrings().fromSubscription(subscriptionData));
+
+        //PCollection<String> pubsubmessage=pipeline.apply(PubsubIO.readStrings().fromTopic("projects/nttdata-c4e-bde/topics/uc1-input-topic-15"));
+
+
+        PCollectionTuple rowcheck=pubsubmessage.apply(ParDo.of(new RowChecker()).withOutputTags(VALID_ROWS, TupleTagList.of(INVALID_ROWS)));
+        PCollection<TableRow> bqrow=rowcheck.get(VALID_ROWS).apply(ParDo.of(new ConvertorStringBq()));
         bqrow.apply(BigQueryIO.writeTableRows().to("nttdata-c4e-bde.uc1_15.account")
                 .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER)
                         .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
+
+        PCollection<String> invalidData=rowcheck.get(INVALID_ROWS);
+        //write Invalid data(malformed data) to Big query.
+        invalidData.apply("SendInValidDataToDLQ",PubsubIO.writeStrings().to(dlqTopicName));
 
         pipeline.run();
     }
@@ -38,6 +56,30 @@ public class Streaming {
         }
 
 
+
+    }
+
+    private static class RowChecker extends DoFn<String,String>{
+
+        private static TupleTag<String> VALID_ROWS=new TupleTag<>(){};
+        private static TupleTag<String> INVALID_ROWS=new TupleTag<>(){};
+        @ProcessElement
+        public void check(ProcessContext processContext){
+            String[] arrJson=processContext.element().split(",");
+            if(arrJson.length==3) {
+                //validatios
+                if(arrJson[0].contains("id") && arrJson[1].contains("name") &&arrJson[2].contains("surname")){
+                    processContext.output(VALID_ROWS,processContext.element());
+                }else{
+                    //Malformed data
+                    processContext.output(INVALID_ROWS,processContext.element());
+                }
+            }else{
+                //Malformed data
+                processContext.output(INVALID_ROWS,processContext.element());
+            }
+
+        }
 
     }
 }
